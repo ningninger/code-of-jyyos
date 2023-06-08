15fbc55cb27ce811ed7a934f2901afd123936b6a
ning <211220110@smail.nju.edu.cn>
2023-06-08 21:43:31 +0800

    save

---
+++ 1-concurrency/1/4-sum/sum.c
@@ -0,0 +1,31 @@
+#include "thread.h"
+
+
+#define N 100000000
+
+
+long sum = 0;//共享内存
+
+
+void Tsum() {
+
+  for (int i = 0; i < N; i++) {
+
+    sum++;
+
+  }
+
+}
+
+
+int main() {
+
+  create(Tsum);
+
+  create(Tsum);
+
+  join();
+
+  printf("sum = %ld\n", sum);
+
+}
